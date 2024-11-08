f85437bae0265e7fbe7fc7ec7b3ba7904d336ffc
byeol <kimbyul5633@gmail.com>
2024-11-09 08:24:05 +0900

    2024.11.09

---
+++ src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Post/>
+    <Post/>
+    <Post/>
+
+  </div>
+</template>
+
+<script>
+import Post from "@/components/Post.vue"
+export default {
+    name : "ContainerComponent",
+    components : {
+        Post,
+    }
+}
+</script>
+
+<style>
+
+</style>
